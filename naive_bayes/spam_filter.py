1b056731a8cde450c496e0e4cf35e19a7590cd3c
haskfp <lispocoder@gmail.com>
2016-08-15 00:35:54 +0300

    Initial Naive Bayes Spam filter.

---
+++ naive_bayes/spam_filter.py
@@ -0,0 +1,147 @@
+#!/usr/bin/python3
+import pandas as pd
+import numpy as np
+import string
+import nltk
+
+import itertools
+from collections import defaultdict
+from collections import namedtuple
+
+from math import log
+
+
+class DataParser(object):
+
+    def __init__(self):
+        self._df = None
+        self.load()
+        self.prepare()
+
+    @property
+    def data(self):
+        """DataFrame that represents the spam corpus."""
+        return self._df
+
+    # TODO: download directly
+    def load(self):
+        sms_spam_corpus = './sms_spam_corpus_01/english_big.txt'
+        self._df = pd.read_csv(sms_spam_corpus,
+                               delimiter='|',
+                               encoding='latin1')
+
+    def prepare(self):
+        self._df['body'] =  self._df['body'].\
+            str.replace(r'[0-9]+', '1').\
+            str.translate(str.maketrans(string.punctuation,
+                          ' ' * len(string.punctuation))).\
+            str.replace(r'[^\x00-\x7F]+', ' ').\
+            str.lower()
+
+
+class Separator(object):
+
+    def __init__(self, data, field):
+        self._train, self._test = self._make_separation(data=data,
+                                                        field=field,
+                                                        train_percent=0.8)
+
+    @property
+    def test(self):
+        """Test part of dataset."""
+        return self._train
+
+    @property
+    def train(self):
+        """Train part of dataset."""
+        return self._test
+
+    def _make_separation(self, data, field, train_percent):
+        msk = np.random.rand(len(data)) < train_percent
+        data[field] = data[field].apply(nltk.word_tokenize)
+        data[field] = data[field].apply(np.array)
+        return data[msk], data[~msk]
+
+
+class NaiveBayesModel(object):
+
+    def __init__(self, train):
+        self._model = None
+        self._fit_model(train_set=train)
+
+    @property
+    def trained_model(self):
+        """Trained Naive Bayes model."""
+        return self._model
+
+    @staticmethod
+    def _create_words_map(words, counts):
+        words_map = defaultdict(int)
+        for i in range(len(words)):
+            words_map[words[i]] = counts[i]
+        return words_map
+
+    def _fit_model(self, train_set):
+        lists_spam_words = train_set[train_set['class'] == 'spam']['body']
+        all_spam_words = np.array(list(itertools.chain(*lists_spam_words)))
+
+        lists_ham_words = train_set[train_set['class'] == 'ham']['body']
+        all_ham_words = np.array(list(itertools.chain(*lists_ham_words)))
+
+        spam_count_map = self._create_words_map(*np.unique(all_spam_words,
+                                                           return_counts=True))
+        ham_count_map = self._create_words_map(*np.unique(all_ham_words,
+                                                          return_counts=True))
+
+        spam_words = np.unique(all_spam_words)
+        ham_words = np.unique(all_ham_words)
+
+        ham = namedtuple('HamModel', ['words', 'count_map'])
+        spam = namedtuple('SpamModel', ['words', 'count_map'])
+        self._model = ham(ham_words, ham_count_map), spam(spam_words, spam_count_map)
+
+
+class NaiveBayesClassifier(object):
+
+    def __init__(self, model):
+        self.model = model
+
+    def get_words(self, msg):
+        prepared_msg = msg.replace(r'[0-9]+', '1').\
+            translate(None, string.punctuation.translate(None, '"')).\
+            replace(r'[^\x00-\x7F]+', ' ').\
+            lower()
+        return np.array(nltk.word_tokenize(prepared_msg))
+
+    def classify_all(self, body):
+        pass
+
+    def classify(self, msg):
+        ham, spam = self.model
+        l_ham = len(ham[0])
+        l_spam = len(spam[0])
+
+        p_ham = log(l_ham) / (log(l_ham) + log(l_spam))
+        p_spam = log(l_spam) / (log(l_ham) + log(l_spam))
+
+        l_all_spam = len(spam[1])
+        l_all_ham = len(ham[1])
+        for word in msg:
+            if ham[1][word]:
+                p_ham += log(ham[1][word]/l_all_spam)
+            if spam[1][word]:
+                p_spam += log(spam[1][word]/l_all_ham)
+            #print('spam: %s, ham: %s, for msg: %s' % (p_ham, p_spam, str(msg)))
+
+        return 'ham' if p_ham > p_spam else 'spam'
+
+    def test_model(self, test_set):
+        return test_set.assign(predict_class=lambda row: self.classify_all(row.body))
+
+if __name__ == "__main__":
+    # TODO: make composition
+    parser = DataParser()
+    separated = Separator(parser.data, 'body')
+    trained_model = NaiveBayesModel(separated.train).trained_model
+    classifier = NaiveBayesClassifier(trained_model)
+    classifier.test_model(separated.test)
